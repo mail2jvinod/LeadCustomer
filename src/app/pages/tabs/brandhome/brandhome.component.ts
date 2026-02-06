45730327297e55403e51ced4b428a9b71be20f4a
mail2jvinod <mail2jvinod@gmail.com>
2026-02-06 08:03:45 +0530

    brandcategory

--- src/app/pages/tabs/brandhome/brandhome.component.ts
+++ src/app/pages/tabs/brandhome/brandhome.component.ts
@@ -1,19 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal, inject, computed } from '@angular/core';
 import { IonContent, IonIcon, IonListHeader, IonToolbar, IonBackButton, IonFabButton, IonButtons, IonHeader } from "@ionic/angular/standalone";
+import { Category } from '../../../interfaces/master-entity';
+import { CategoryService } from '../../../services/category/category.service';
+import { Banner } from '../../../interfaces/banner.interface';
+import { BannerService } from '../../../services/banner/banner.service';
+import { BannerComponent } from '../../../components/banner/banner.component';
+import { CategoriesComponent } from '../../../components/categories/categories.component';
+import { ListHeadingComponent } from '../../../components/list-heading/list-heading.component';
 
 @Component({
-  selector: 'app-brandhome',
-  templateUrl: './brandhome.component.html',
-  styleUrls: ['./brandhome.component.scss'],
-  imports: [IonHeader, IonButtons,
-    IonBackButton,
-    IonContent,
-    IonToolbar],
+    selector: 'app-brandhome',
+    templateUrl: './brandhome.component.html',
+    styleUrls: ['./brandhome.component.scss'],
+    imports: [IonHeader, IonButtons,
+        IonBackButton,
+        IonContent,
+        IonToolbar,
+        ListHeadingComponent,
+        BannerComponent,
+        CategoriesComponent,
+    ],
 })
-export class BrandHomeComponent  implements OnInit {
+export class BrandHomeComponent implements OnInit {
 
-  constructor() { }
+    private categoryService = inject(CategoryService);
+    private bannerService = inject(BannerService);
 
-  ngOnInit() {}
+    catgList = signal<Category[] | null>([]);
+    banners = computed<Banner[]>(() => this.bannerService.getBanners());
+    constructor() { }
+
+    ngOnInit() { }
+
+    async ngAfterViewInit() {
+        this.categoryService.getCategories(0).subscribe(
+            {
+                next: (data) => {
+                    this.catgList.set(data.filter(f => f.ParentCategoryId === 0));
+                    //this.subCatgList.set(data.filter(f => f.ParentCategoryId > 0));
+                }
+            }
+        )
+
+
+
+    }
+
+
+
 
 }
--- src/app/pages/tabs/categoryhome/categoryhome.component.ts
+++ src/app/pages/tabs/categoryhome/categoryhome.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal, inject, computed } from '@angular/core';
 import { IonContent, IonIcon, IonListHeader, IonToolbar, IonBackButton, IonFabButton, IonButtons, IonHeader } from "@ionic/angular/standalone";
+import { CategoryService } from '../../../services/category/category.service';
+import { Brand } from '../../../interfaces/master-entity';
+import { BannerService } from '../../../services/banner/banner.service';
+import { Banner } from '../../../interfaces/banner.interface';
+import { ListHeadingComponent } from '../../../components/list-heading/list-heading.component';
+import { BannerComponent } from '../../../components/banner/banner.component';
+import { BrandsComponent } from '../../../components/brands/brands.component';
 
 @Component({
-  selector: 'app-categoryhome',
-  templateUrl: './categoryhome.component.html',
-  styleUrls: ['./categoryhome.component.scss'],
-  imports: [IonHeader, IonButtons,
-      IonBackButton,
-      IonContent,
-      IonToolbar],
+    selector: 'app-categoryhome',
+    templateUrl: './categoryhome.component.html',
+    styleUrls: ['./categoryhome.component.scss'],
+    imports: [IonHeader, IonButtons,
+        IonBackButton,
+        IonContent,
+        IonToolbar,
+        ListHeadingComponent,
+        BannerComponent,
+        BrandsComponent,
+    ],
 })
-export class CategoryHomeComponent  implements OnInit {
+export class CategoryHomeComponent implements OnInit {
+    private categoryService = inject(CategoryService);
+    private bannerService = inject(BannerService);
 
-  constructor() { }
+    banners = computed<Banner[]>(() => this.bannerService.getBanners());
+    brandsList = signal<Brand[] | null | undefined>([]);
 
-  ngOnInit() {}
 
+    constructor() { }
+
+    ngOnInit() { }
+    async ngAfterViewInit() {
+        this.categoryService.getBrands(0).subscribe(
+            {
+                next: (data) => {
+                    this.brandsList.set(data);
+                }
+            }
+        )
+
+
+
+    }
 }
